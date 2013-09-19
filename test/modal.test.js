841053bdaed0ff2459ab7d21b7670c9898e5cc07
Ben Gourley <bn@grly.me>
2013-09-19 14:27:06 +0100

    Fix tests to be compatible with $(window).height()

--- test/modal.test.js
+++ test/modal.test.js
@@ -132,7 +132,11 @@ describe('modal', function () {
     it('should centre to window height', function () {
       var m = modal({ fx: false })
       assert.equal(typeof m.centre, 'function')
-      window.innerHeight = 1000
+
+      // jQuery requires a bit of persuasion the window really
+      // is 1000px tall, hence these two assignments
+      window.innerHeight = document.documentElement.clientHeight = 1000
+
       $('.modal-content').height(100)
       assert.equal($('.modal-content').outerHeight(), 100)
       $('.modal-content').css('top', 0)
